3e2b0744e15906af807b479f8f4666bb914bccf7
Denys Klymenko <denis.klimenko.dx@gmail.com>
2021-06-15 18:00:58 +0300

    add cognitive bias article

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,10 @@ const IndexPage = () => {
 
         <h3>Все статьи</h3>
         <ul>
-          <li><a href={'/articles/parasympathetic-breath'}>Парасимпатическое дыхание для увеличения силы воли</a></li>
-          <li><a href={'/articles/inner-trigger'}>Триггер - только внутренний</a></li>
+          <li><a href={'/articles/cognitive-bias'}>Когнитивные искажения</a></li>
           <li><a href={'/articles/theory-of-mind'}>Theory of Mind (ToM) / чтение мыслей, чувств / интуиция</a></li>
+          <li><a href={'/articles/inner-trigger'}>Триггер - только внутренний</a></li>
+          <li><a href={'/articles/parasympathetic-breath'}>Парасимпатическое дыхание для увеличения силы воли</a></li>
         </ul>
       </main>
     </Layout>
